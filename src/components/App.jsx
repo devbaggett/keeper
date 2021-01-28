ae341550efc13735f364b1050258ce4216f7fe2d
devbaggett <devabaggett@gmail.com>
2021-01-28 13:30:10 -0800

    Refactored Card component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,17 @@ import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
 
-function createNote(note) {
-    return (
-        <Note
-            key={note.key}
-            title={note.title}
-            content={note.content}
-        />
-    );
-}
-
 function App() {
     return (
         <div>
             <Header />
-            {notes.map(createNote)}
+            {notes.map(note => (
+                <Note
+                    key={note.key}
+                    title={note.title}
+                    content={note.content}
+                />
+            ))}
             <Footer />
         </div>
     );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -6,7 +6,7 @@ function Note(props) {
             <h1>{props.title}</h1>
             <p>{props.content}</p>
         </div>
-    )
+    );
 }
 
 export default Note;
